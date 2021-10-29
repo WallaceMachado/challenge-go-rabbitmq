458419946967f37d38646bbf9b85908ef248ad60
wallacemachado <wallacefm18@gmail.com>
2021-10-29 17:26:41 -0300

    add model person

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/wallacemachado/challenge-go-rabbitmq
 go 1.16
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.4.0
+	github.com/satori/go.uuid v1.2.0
 	go.mongodb.org/mongo-driver v1.7.3
 )
---
+++ models/Person.go
@@ -0,0 +1,74 @@
+package models
+
+import (
+	"strings"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+type Person struct {
+	ID        string    `bson:"_id,omitempty" json:"id,omitempty" valid:"notnull,uuid"`
+	Name      string    `bson:"name" json:"name"`
+	Gender    string    `bson:"gender" json:"gender"`
+	Weight    float64   `bson:"weight" json:"weight" valid:"notnull,float"`
+	Height    float64   `bson:"height" json:"height" valid:"notnull,float"`
+	IMC       float64   `bson:"imc" json:"imc" valid:"notnull,float"`
+	CreatedAt time.Time `bson:"created_at,omitempty" json:"createdAt" valid:"-"`
+	UpdatedAt time.Time `bson:"updated_at,omitempty" json:"updatedAt" valid:"-"`
+}
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+func NewPerson(name string, gender string, weight float64, height float64, imc float64) (*Person, error) {
+	person := &Person{
+		Name:   name,
+		Gender: gender,
+		Weight: weight,
+		Height: height,
+		IMC:    imc,
+	}
+
+	person.ID = uuid.NewV4().String()
+	person.CreatedAt = time.Now()
+	person.UpdatedAt = time.Now()
+
+	err := person.prepare()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return person, nil
+}
+
+func (p *Person) prepare() error {
+
+	p.Name = strings.TrimSpace(p.Name)
+	p.Name = strings.ToLower(p.Name)
+	p.Gender = strings.TrimSpace(p.Gender)
+	p.Gender = strings.ToLower(p.Gender)
+
+	err := p.validate()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func (p *Person) validate() error {
+
+	_, err := govalidator.ValidateStruct(p)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
